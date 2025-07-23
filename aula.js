bf1d0e852b23d914ce85ace835196b40d7b0341a
limamario441 <limamario441@gmail.com.br>
2025-07-23 15:20:28 -0300

    condiçoes

---
+++ aula.js
@@ -0,0 +1,13 @@
+
+
+
+
+const use = "jonas";
+const password = '123456';
+
+const vaiLogar = use === 'jonas' && password === '12345';
+
+console.log(vaiLogar);
+
+
+console.log('julio' && 0 && 'tinga');
---
+++ aula2.js
@@ -0,0 +1,11 @@
+const hs = 50;
+
+if (hs >= 0 && hs <= 11) {
+  console.log('bom dia!!');
+} else if (hs >= 12 && hs <= 17) {
+  console.log('boa tade!!');
+} else if (hs >= 18 && hs <= 23) {
+  console.log('boa noite!!');
+} else {
+    console.log('falha!!!!!')
+}
